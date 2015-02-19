e3019d3b2bdeb7b0f01561f91eefef774a758e5b
maxwen <max.weninger@gmail.com>
2015-02-19 15:15:49 -0800

    Add OmniSwitch as option for default recents [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3808,6 +3808,12 @@ public final class Settings {
         public static final String MENU_VISIBILITY = "menu_visibility";
 
         /**
+         * Alternative recent apps integration using OmniSwitch
+         * @hide
+         */
+        public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
