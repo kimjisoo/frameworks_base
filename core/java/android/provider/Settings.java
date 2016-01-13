c9f49f628217ad96ade376d32493371d51ae64f7
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-01-13 17:20:29 +0100

    [1/2] Base: Slim Global Menu
    Main work by fusionjack <dogfight60-fusionjack@yahoo.de>.
    
    Forward port from SlimKat to SlimLP
    
    Forward port from SlimLP to AOSP MM by Yank555.lu
    
    - rely on ActionHelper for non global menu specific icons
    
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3240,6 +3240,13 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_ANYWHERE = "double_tap_sleep_anywhere";        /**
 
         /**
+         * Config for advanced power menu
+         *
+         * @hide
+         */
+        public static final String POWER_MENU_CONFIG = "power_menu_config";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
