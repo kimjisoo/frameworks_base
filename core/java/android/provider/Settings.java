b44cbc29d121a8e79f7ff3223958c1efb4c38be8
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-11-25 21:54:23 -0600

    SystemUI: Add back the Pixel Navbar
    - Shoutout to @bigrushdog for providing a decompiled form used
      to verify the smali and as a guideline for the more obsure
      smali sections. It was a big help.
    - Tagging myself here to see adoption rate. @USA-RedDragon
    
    @mydongistiny: Squashed with:
    - Fix landscape pixel animation view @USA-RedDragon
    - Cleanup pixel navbar code @BeansTown106
    - Pixel navbar: remove this. and OpaLayout references, cleanup @KreAch3R
    - Pixel navbar: allow disabling based on config @KreAch3R
    - fwb: Pixel Navbar: Hide opa when not pressed @Myself5
    - systemui: opalayout: fix up build error with latest source @mydongistiny
    
    Change-Id: I6ed0979441bd7048b71a2d2edebe0bbc90bf3829
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    PixelNavbar: Fixed to work with oreo
    
    Change-Id: I84f5953d7fe05f26ec347f78be9a73e1ebf95398
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Pixel Navbar animation toggle [1/2]
    
    *made to work with the exposure for themers to disable navbar animation
    
    Change-Id: If15aff30447a65b9199d1768802ce86f1e1e1615
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Reload Pixel Home Animation without reboot
    
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4061,6 +4061,12 @@ public final class Settings {
         public static final String QS_EXPAND_INDICATOR_TOGGLE = "qs_expand_indicator_toggle";
 
         /**
+         * Whether to enable the pixel navbar animation
+         * @hide
+         */
+        public static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
