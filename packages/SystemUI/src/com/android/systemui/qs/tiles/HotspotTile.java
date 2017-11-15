47dd1a1e086e39386a45a9d218ac9f4d9b140b1f
kimjisoo <kimjisoo.android@gmail.com>
2017-11-15 13:30:04 +0900

    hotspot tile label should follow state

--- packages/SystemUI/src/com/android/systemui/qs/tiles/HotspotTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/HotspotTile.java
@@ -113,6 +113,12 @@ public class HotspotTile extends QSTileImpl<AirplaneBooleanState> {
             state.slash = new SlashState();
         }
         state.label = mContext.getString(R.string.quick_settings_hotspot_label);
+        final boolean labelOn = mController.isHotspotEnabled();
+        if (labelOn) {
+            state.label = mContext.getString(R.string.quick_settings_hotspot_on);
+        } else {
+            state.label = mContext.getString(R.string.quick_settings_hotspot_off);
+        }
 
         checkIfRestrictionEnforcedByAdminOnly(state, UserManager.DISALLOW_CONFIG_TETHERING);
         if (arg instanceof Boolean) {
