05af25bd29c7daa53e898b790af596257adecdfd
kimjisoo <kimjisoo.android@gmail.com>
2018-02-13 15:52:59 +0900

    improve WiFi QS strings

--- packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/WifiTile.java
@@ -166,18 +166,18 @@ public class WifiTile extends QSTileImpl<SignalState> {
         final Resources r = mContext.getResources();
         if (cb.isTransient) {
             state.icon = ResourceIcon.get(R.drawable.ic_signal_wifi_transient_animation);
-            state.label = r.getString(R.string.quick_settings_wifi_label);
+            state.label = r.getString(R.string.quick_settings_wifi_not_connected);
         } else if (!state.value) {
             state.slash.isSlashed = true;
             state.state = Tile.STATE_INACTIVE;
             state.icon = ResourceIcon.get(R.drawable.ic_qs_wifi_disabled);
-            state.label = r.getString(R.string.quick_settings_wifi_label);
+            state.label = r.getString(R.string.quick_settings_wifi_off);
         } else if (wifiConnected) {
             state.icon = ResourceIcon.get(cb.wifiSignalIconId);
             state.label = removeDoubleQuotes(cb.enabledDesc);
         } else if (wifiNotConnected) {
             state.icon = ResourceIcon.get(R.drawable.ic_qs_wifi_disconnected);
-            state.label = r.getString(R.string.quick_settings_wifi_label);
+            state.label = r.getString(R.string.quick_settings_wifi_not_connected);
         } else {
             state.icon = ResourceIcon.get(R.drawable.ic_qs_wifi_no_network);
             state.label = r.getString(R.string.quick_settings_wifi_label);
